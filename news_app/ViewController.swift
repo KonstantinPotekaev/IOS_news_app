cfb56c0c3b8f6cc0958c353960e69bf7a30dd918
Konstantin Potekaev <Konstantin.Potekaev@infowatch.com>
2024-12-12 10:10:08 +0300

    Initial Commit

---
+++ news_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news_app
+//
+//  Created by Константин Потекаев on 12.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
